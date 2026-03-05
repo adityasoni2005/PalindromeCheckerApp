579fa9375b44b3edea9ba83bbc7b7283d2c7de1b
adityasoni2005 <aadilovemaa@gmail.com>
2026-03-05 09:51:19 +0530

    UC7

---
+++ src/UseCase7PCA.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PCA {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Initialize Deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        // Compare front and rear
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
