2d09866203cfbdbd26346b574eba4a3a6e89e51f
adityasoni2005 <aadilovemaa@gmail.com>
2026-03-05 09:40:15 +0530

    UC2:Hardcoded palindrome

---
+++ src/UseCase4PCA.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class UseCase4PCA {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        // Convert string to character array
+        char[] characters = input.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
